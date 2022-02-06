d654ad78ec92706e5f4093e5fa78c4436e0586d6
jewishmoses <work@xmoses.com>
2022-02-06 15:38:45 +0200

    added connection to database

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 require('dotenv/config');
 const app = require('./app');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT;
+
+mongoose.connect(process.env.DB_CONNECTION, () => {
+
+    const isConnected = mongoose.connection.readyState == 1;
+    isConnected && console.log('Connected to DB.');
+    isConnected == false && console.log('Connection to DB failed.');
+
+});
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Running at http://0.0.0.0:${PORT}`);
