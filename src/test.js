2d89d759b1a85ea5450f85caeb562beb591e754b
jewishmoses <work@xmoses.com>
2022-02-06 19:55:06 +0200

    added & improved tests

--- src/test.js
+++ src/test.js
@@ -3,7 +3,34 @@ const app = require('./app');
 const request = require('supertest');
 const mongoose = require('mongoose');
 const User = require('./models/User');
+const Report = require('./models/Report');
 const nock = require('nock');
+
+const mockLoginRequest = () => {
+
+    const headers = {
+        'set-cookie': 'AppCookie=AAAA;'
+    };
+
+    const body = {
+        isUserAuth: true,
+        isCommanderAuth: false,
+        error: null,
+    };
+
+    nock('https://one.prat.idf.il')
+        .post('/api/account/login')
+        .reply(200, body, headers);
+
+};
+
+const getToken = async ({ username, password, recaptchaValue }) => {
+
+    const data = { username, password, recaptchaValue };
+    const response = await request(app).post('/users/login').send(data);
+    return response.body.data.token;
+
+}
 
 beforeAll(async () => {
 
@@ -65,20 +92,7 @@ describe('Post to /users/login', () => {
 
     it("users can register", async () => {
 
-        const headers = {
-            'set-cookie': 'AppCookie=AAAA;'
-        };
-
-        const body = {
-            isUserAuth: true,
-            isCommanderAuth: false,
-            error: null,
-        }
-
-        nock('https://one.prat.idf.il')
-            .post('/api/account/login')
-            .reply(200, body, headers);
-
+        await mockLoginRequest();
         const data = { username: '123456787', password: '123456', recaptchaValue: 'ABCD' };
         const response = await request(app).post('/users/login').send(data);
         expect(response.status).toBe(200);
@@ -89,20 +103,7 @@ describe('Post to /users/login', () => {
 
     it("users can login", async () => {
 
-        const headers = {
-            'set-cookie': 'AppCookie=AAAA;'
-        };
-
-        const body = {
-            isUserAuth: true,
-            isCommanderAuth: false,
-            error: null,
-        }
-
-        nock('https://one.prat.idf.il')
-            .post('/api/account/login')
-            .reply(200, body, headers);
-
+        await mockLoginRequest();
         const data = { username: '123456789', password: '123456', recaptchaValue: 'ABCD' };
         const response = await request(app).post('/users/login').send(data);
         expect(response.status).toBe(200);
@@ -124,28 +125,78 @@ describe('Get to /users/me', () => {
 
     it("logged in users can access route", async () => {
 
-        const headers = {
-            'set-cookie': 'AppCookie=AAAA;'
-        };
-
-        const body = {
-            isUserAuth: true,
-            isCommanderAuth: false,
-            error: null,
-        }
-
-        nock('https://one.prat.idf.il')
-            .post('/api/account/login')
-            .reply(200, body, headers);
-
+        await mockLoginRequest();
         const data = { username: '123456789', password: '123456', recaptchaValue: 'ABCD' };
-        let response = await request(app).post('/users/login').send(data);
-        expect(response.status).toBe(200);
+        const token = await getToken(data);
 
-        const token = response.body.data.token;
         response = await request(app).get('/users/me').set('Authorization', `Bearer ${token}`);
         expect(response.status).toBe(200);
         expect(response.body).toMatchObject({ username: '123456789' });
+
+    });
+
+});
+
+describe("Post to /reports", () => {
+
+    it("user must be logged in", async () => {
+
+        const response = await request(app).post('/reports');
+        expect(response.status).toBe(403);
+
+    });
+
+    it("route is working", async () => {
+
+        await mockLoginRequest();
+        let data = { username: '123456789', password: '123456', recaptchaValue: 'ABCD' };
+        const token = await getToken(data);
+
+        data = {
+            "sunday": {
+                "type": "0101",
+                "hour": 6,
+            }
+        }
+
+        response = await request(app).post('/reports').set('Authorization', `Bearer ${token}`).send(data);
+        expect(response.status).toBe(200);
+
+    });
+
+    it("hour is required", async () => {
+
+        await mockLoginRequest();
+        let data = { username: '123456789', password: '123456', recaptchaValue: 'ABCD' };
+        const token = await getToken(data);
+
+        data = {
+            "sunday": {
+                "type": "0101",
+            }
+        }
+
+        response = await request(app).post('/reports').set('Authorization', `Bearer ${token}`).send(data);
+        expect(response.status).toBe(400);
+
+    });
+
+    it("type is required", async () => {
+
+        await mockLoginRequest();
+        let data = { username: '123456789', password: '123456', recaptchaValue: 'ABCD' };
+        const token = await getToken(data);
+
+        data = {
+            "sunday": {
+                "hour": 6,
+            }
+        }
+
+        response = await request(app).post('/reports').set('Authorization', `Bearer ${token}`).send(data);
+        expect(response.status).toBe(400);
+
+        // TODO: also check with the database
 
     });
 
