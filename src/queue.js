dc0ceaf103e997988364176a7afd96b8d9ba3a32
jewishmoses <work@xmoses.com>
2022-02-07 12:58:33 +0200

    using node-schedule & added delay to report job

--- src/queue.js
+++ src/queue.js
@@ -1,9 +1,12 @@
 require('dotenv/config');
 const Queue = require('bull');
 const Report = require('./models/Report');
-var path = require("path");
+var path = require('path');
 const DB = require('./database');
 const redis = require('../config/redis');
+const schedule = require('node-schedule');
+const { randomInteger } = require('./helpers/utilities');
+const dayjs = require('dayjs');
 
 // Report job
 
@@ -16,7 +19,13 @@ reportQueue.process(reportJob);
 const resetReportsQueue = new Queue('B', { redis });
 const resetReportsJob = path.resolve('src', 'jobs', 'resetReports.js');
 resetReportsQueue.process(resetReportsJob);
-resetReportsQueue.add({}, { repeat: { cron: '15 10 * * 4' } }); // At 10:15 on Thursday.
+
+schedule.scheduleJob('15 10 * * 4', () => {
+
+    resetReportsQueue.add(); // At 10:15 on Thursday.
+
+});
+
 
 // Schedule reports job
 
@@ -34,21 +43,31 @@ scheduleReportsQueue.process(async () => {
 
         const report = reports[i];
         const reportType = report[currentDay]['type'];
-
-        console.log({ user: report.user, reportType });
-
-        reportQueue.add({ user: report.user, reportType }); // TODO: add correct delay
+        const reportHour = report[currentDay]['hour'];
+        const reportMinute = randomInteger(0, 59);
+        const delay = dayjs().hour(reportHour).minute(reportMinute).valueOf() - dayjs().valueOf();
+        reportQueue.add({ user: report.user, reportType }, { delay });
 
     }
 
     await DB.disconnect();
 
 });
-scheduleReportsQueue.add({}, { repeat: { cron: '40 4 * * 0,1,2,3,4 ' } }); // At 04:40 on Sunday, Monday, Tuesday, Wednesday, and Thursday.
+
+schedule.scheduleJob('40 4 * * 0,1,2,3,4 ', () => {
+
+    scheduleReportsQueue.add(); // At 04:40 on Sunday, Monday, Tuesday, Wednesday, and Thursday.
+
+});
 
 // WhatsApp job
 
 const WhatsAppQueue = new Queue('D', { redis });
 const WhatsAppJob = path.resolve('src', 'jobs', 'whatsapp.js');
 WhatsAppQueue.process(WhatsAppJob);
-WhatsAppQueue.add({}, { repeat: { cron: '0 6,7,8,9,10 * * 0,1,2,3,4' } }); // At minute 0 past hour 6, 7, 8, 9, and 10 on Sunday, Monday, Tuesday, Wednesday, and Thursday.
+
+schedule.scheduleJob('0 6,7,8,9,10 * * 0,1,2,3,4', () => {
+
+    WhatsAppQueue.add(); // At minute 0 past hour 6, 7, 8, 9, and 10 on Sunday, Monday, Tuesday, Wednesday, and Thursday.
+
+});
