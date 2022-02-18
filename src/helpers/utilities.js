5dfcd92fd7ccb1d793912f6450b4c597d08106c2
jewishmoses <work@xmoses.com>
2022-02-18 21:43:49 +0200

    added formatSeconds func

--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -6,4 +6,21 @@ function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-module.exports = { sleep, randomInteger };
+function formatSeconds(seconds) {
+
+    var sec_num = parseInt(seconds, 10);
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - hours * 3600) / 60);
+    var seconds = sec_num - hours * 3600 - minutes * 60;
+
+    if (hours < 10) { hours = "0" + hours; }
+
+    if (minutes < 10) { minutes = "0" + minutes; }
+
+    if (seconds < 10) { seconds = "0" + seconds; }
+
+    return hours + ":" + minutes + ":" + seconds;
+
+}
+
+module.exports = { sleep, randomInteger, formatSeconds };
